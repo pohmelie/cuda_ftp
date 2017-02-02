82728b57edac507c9ee4f150d0427f2096760c46
Alex <Alexey-T@users.noreply.github.com>
2017-02-03 03:06:32 +0400

    dialog improved

--- dlg.py
+++ dlg.py
@@ -12,48 +12,59 @@ def dialog_server_props(s_type, s_host, s_port, s_username, s_password, s_dir, s
     RES_PORT = 6
     RES_USERNAME = 8
     RES_PASS = 10
-    RES_PASS_ASK = 11
-    RES_DIR = 13
-    RES_TIMEOUT = 15 
-    RES_LABEL = 17
-    RES_OK = 18
+    RES_PASS_DEFAULT = 11
+    RES_PASS_ASK = 12
+    RES_DIR = 14
+    RES_TIMEOUT = 16 
+    RES_LABEL = 18
+    RES_OK = 19
     
     c1 = chr(1)
-    res = dlg_custom('FTP server info', 496, 360, 
-      '\n'.join([]
-         +[c1.join(['type=label', 'pos=6,8,190,0', 'cap=Server type:'])]
-         +[c1.join(['type=radio', 'pos=180,6,290,0', 'cap=FTP', 'val='+('1' if s_type=='ftp' else '0') ])]
-         +[c1.join(['type=radio', 'pos=250,6,390,0', 'cap=SFTP', 'val='+('1' if s_type=='sftp' else '0')])]
+    while True:
+        res = dlg_custom('FTP server info', 496, 360, 
+          '\n'.join([]
+             +[c1.join(['type=label', 'pos=6,8,190,0', 'cap=Server type:'])]
+             +[c1.join(['type=radio', 'pos=180,6,290,0', 'cap=FTP', 'val='+('1' if s_type=='ftp' else '0') ])]
+             +[c1.join(['type=radio', 'pos=250,6,390,0', 'cap=SFTP', 'val='+('1' if s_type=='sftp' else '0')])]
          
-         +[c1.join(['type=label', 'pos=6,38,148,0', 'cap=Host (e.g. ftp.site.com):'])]
-         +[c1.join(['type=edit', 'pos=180,36,490,0', 'val='+s_host])]
+             +[c1.join(['type=label', 'pos=6,38,148,0', 'cap=Host (e.g. ftp.site.com):'])]
+             +[c1.join(['type=edit', 'pos=180,36,490,0', 'val='+s_host])]
 
-         +[c1.join(['type=label', 'pos=6,68,148,0', 'cap=Port:'])]
-         +[c1.join(['type=edit', 'pos=180,66,240,0', 'val='+s_port])]
+             +[c1.join(['type=label', 'pos=6,68,148,0', 'cap=Port:'])]
+             +[c1.join(['type=edit', 'pos=180,66,240,0', 'val='+s_port])]
 
-         +[c1.join(['type=label', 'pos=6,98,148,0', 'cap=Username:'])]
-         +[c1.join(['type=edit', 'pos=180,96,490,0', 'val='+s_username])]
+             +[c1.join(['type=label', 'pos=6,98,148,0', 'cap=Username:'])]
+             +[c1.join(['type=edit', 'pos=180,96,490,0', 'val='+s_username])]
 
-         +[c1.join(['type=label', 'pos=6,128,148,0', 'cap=Password:'])]
-         +[c1.join(['type=edit_pwd', 'pos=180,126,490,0', 'val='+(s_password if s_password!='?' else '') ])]
-         +[c1.join(['type=check', 'pos=180,156,490,0', 'cap=Ask password every time', 'val='+('1' if s_password=='?' else '0') ])]
+             +[c1.join(['type=label', 'pos=6,128,148,0', 'cap=Password:'])]
+             +[c1.join(['type=edit_pwd', 'pos=180,126,490,0', 'val='+(s_password if s_password!='?' else '') ])]
+             +[c1.join(['type=button', 'pos=180,156,380,0', 'cap=Default anonymous login' ])]
+             +[c1.join(['type=check', 'pos=180,186,490,0', 'cap=Ask password every time', 'val='+('1' if s_password=='?' else '0') ])]
 
-         +[c1.join(['type=label', 'pos=6,188,148,0', 'cap=Initial remote dir:'])]
-         +[c1.join(['type=edit', 'pos=180,186,490,0', 'val='+s_dir])]
+             +[c1.join(['type=label', 'pos=6,218,148,0', 'cap=Initial remote dir:'])]
+             +[c1.join(['type=edit', 'pos=180,216,490,0', 'val='+s_dir])]
 
-         +[c1.join(['type=label', 'pos=6,218,148,0', 'cap=Timeout (seconds):'])]
-         +[c1.join(['type=spinedit', 'pos=180,216,240,0', 'props=1,120,1', 'val='+s_timeout])]
+             +[c1.join(['type=label', 'pos=6,248,148,0', 'cap=Timeout (seconds):'])]
+             +[c1.join(['type=spinedit', 'pos=180,246,240,0', 'props=1,120,1', 'val='+s_timeout])]
 
-         +[c1.join(['type=label', 'pos=6,248,148,0', 'cap=Label (for menu):'])]
-         +[c1.join(['type=spinedit', 'pos=180,246,240,0', 'props=1,6,1', 'val='+s_label])]
+             +[c1.join(['type=label', 'pos=6,278,148,0', 'cap=Label (for menu):'])]
+             +[c1.join(['type=spinedit', 'pos=180,276,240,0', 'props=1,6,1', 'val='+s_label])]
 
-         +[c1.join(['type=button', 'pos=300,330,394,0', 'cap=&OK', 'props=1'])]
-         +[c1.join(['type=button', 'pos=400,330,490,0', 'cap=Cancel'])]
-      ) )
-    if res is None: return
-        
-    res, s = res
-    if res != RES_OK: return
+             +[c1.join(['type=button', 'pos=300,330,394,0', 'cap=&OK', 'props=1'])]
+             +[c1.join(['type=button', 'pos=400,330,490,0', 'cap=Cancel'])]
+          ) )
+        if res is None: return
+        res, s = res
+    
+        if res == RES_PASS_DEFAULT:
+            s_username = 'anonymous'
+            s_password = 'user@aol.com'
+            continue
+        elif res == RES_OK:
+            break
+        else:
+            return
+    
     
     s = s.splitlines()
     return (
