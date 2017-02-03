8bf457df87379b36c1d7f1f7c2ffcc8591225ab4
Alex <Alexey-T@users.noreply.github.com>
2017-02-03 06:28:05 +0400

    fixed new dialog

--- dlg.py
+++ dlg.py
@@ -1,4 +1,7 @@
+import os
 from cudatext import *
+
+is_unix = os.name!='nt' 
 
 def dialog_server_props(s_type, s_host, s_port, s_username, s_password, s_dir, s_timeout, s_label):
 
@@ -24,8 +27,8 @@ def dialog_server_props(s_type, s_host, s_port, s_username, s_password, s_dir, s
         res = dlg_custom('FTP server info', 496, 360, 
           '\n'.join([]
              +[c1.join(['type=label', 'pos=6,8,190,0', 'cap=Server type:'])]
-             +[c1.join(['type=radio', 'pos=180,6,290,0', 'cap=FTP', 'val='+('1' if s_type=='ftp' else '0') ])]
-             +[c1.join(['type=radio', 'pos=250,6,390,0', 'cap=SFTP', 'val='+('1' if s_type=='sftp' else '0')])]
+             +[c1.join(['type=radio', 'pos=180,6,250,0', 'cap=FTP', 'val='+('1' if s_type=='ftp' else '0') ])]
+             +[c1.join(['type=radio', 'pos=250,6,350,0', 'cap=SFTP', 'val='+('1' if s_type=='sftp' else '0'), 'en='+('1' if is_unix else '0') ])]
          
              +[c1.join(['type=label', 'pos=6,38,148,0', 'cap=Host (e.g. ftp.site.com):'])]
              +[c1.join(['type=edit', 'pos=180,36,490,0', 'val='+s_host])]
@@ -55,6 +58,16 @@ def dialog_server_props(s_type, s_host, s_port, s_username, s_password, s_dir, s
           ) )
         if res is None: return
         res, s = res
+        s = s.splitlines()
+        
+        s_type = 'ftp' if s[RES_TYPE_FTP=='1'] else 'sftp' if s[RES_TYPE_SFTP]=='1' else ''
+        s_host = s[RES_HOST]
+        s_port = s[RES_PORT]
+        s_username = s[RES_USERNAME]
+        s_password = s[RES_PASS] if s[RES_PASS_ASK]=='0' else '?'
+        s_dir = s[RES_DIR]
+        s_timeout = s[RES_TIMEOUT]
+        s_label = s[RES_LABEL]
     
         if res == RES_PASS_DEFAULT:
             s_username = 'anonymous'
@@ -62,27 +75,13 @@ def dialog_server_props(s_type, s_host, s_port, s_username, s_password, s_dir, s
             continue
             
         if res == RES_OK:
-            test = s.splitlines()
-            if not test[RES_HOST]:
+            if not s_host:
                 msg_box('Fill the Host field', MB_OK)
                 continue
-            if not test[RES_USERNAME]:
+            if not s_username:
                 msg_box('Fill the Username field', MB_OK)
                 continue
-            break
+            return (s_type, s_host, s_port, s_username, s_password, s_dir, s_timeout, s_label)
             
         else:
             return
-    
-    
-    s = s.splitlines()
-    return (
-        'ftp' if s[RES_TYPE_FTP=='1'] else 'sftp' if s[RES_TYPE_SFTP]=='1' else '',
-        s[RES_HOST],
-        s[RES_PORT],
-        s[RES_USERNAME],
-        s[RES_PASS] if s[RES_PASS_ASK]=='0' else '?',
-        s[RES_DIR],
-        s[RES_TIMEOUT],
-        s[RES_LABEL]
-        )
